b503907d086815ded441d2e8195961e44c00b899
anuj <52917153+arrayweb@users.noreply.github.com>
2021-08-26 18:40:03 +0530

    add header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,35 @@
 import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import { alpha, makeStyles } from '@material-ui/core/styles';
+import shadows from "@material-ui/core/styles/shadows";
+
+const useStyles = makeStyles((theme) => ({
+  title: {
+    color: "#0c4fff",
+    flexGrow: 1,
+    fontSize: '10px'
+  },
+  trackercount: {
+    color: "#2e3fff",
+    fontSize: '10px'
+  },
+  appBar: {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+    boxShadow: 'none'
+  }
+}));
 
 export default function Header() {
+  const classes = useStyles();
+  const n = 4;
   return (
     <div>
-      <AppBar position='fixed' color="transparent">
+      <AppBar position='sticky' className={classes.appBar} >
         <Toolbar >
-          <Typography variant="h6" style={{ color: "#ec407a", flexGrow: 1, fontSize: '8px' }}>
-            Newsgg
+          <Typography variant="h6" className={classes.title}>
+            Live website tracker
           </Typography>
-          <Typography variant="h6" style={{ color: "#ec407a" }} >
-            News
+          <Typography variant="h6" className={classes.trackercount} >
+            number of website being tracked {n}
           </Typography>
         </Toolbar>
       </AppBar>
